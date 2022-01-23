d62deb1c7440ffcf4e8f9dbb4f3e7703763a9034
OriolGarciaFores <ori_ugf@hotmail.com>
2022-01-23 09:58:39 +0100

    Nuevo proyecto Bot config.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Intents, MessageEmbed, Message } = require('discord.js');
-const { prefix } = require('../config.json');
+const { prefix } = require('./config.json');
 require("dotenv").config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
