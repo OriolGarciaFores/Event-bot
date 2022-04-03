84c42d09fdbb6870fd174699e97b37400865f94a
OriolGarciaFores <ori_ugf@hotmail.com>
2022-04-03 21:51:56 +0200

    Añadido tiempo a los logs.

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -1,23 +1,37 @@
 const COLOR = require('../constants/consoleColors.js');
 
 function info(content){
-    if(process.env.LOG_INFO) console.log(COLOR.BLUE, '[INFO]' + content);
+    if(process.env.LOG_INFO) {
+        let fecha = new Date();
+        console.log(COLOR.BLUE, '[INFO][' + fecha.toLocaleString() + '] ' + content);
+    }
 }
 
 function error(content){
-    if(process.env.LOG_ERROR) console.log(COLOR.RED, '[ERROR]' + content);
+    if(process.env.LOG_ERROR) {
+        console.log(COLOR.RED, '[ERROR][' + fecha.toLocaleString() + '] ' + content);
+    }
 }
 
 function warn(content){
-    if(process.env.LOG_WARN) console.log(COLOR.YELLOW, '[WARN]' + content);
+    if(process.env.LOG_WARN) {
+        let fecha = new Date();
+        console.log(COLOR.YELLOW, '[WARN][' + fecha.toLocaleString() + '] ' + content);
+    }
 }
 
 function correct(content){
-    if(process.env.LOG_SUCCESS) console.log(COLOR.GREEN, '[SUCCESS]' + content);
+    if(process.env.LOG_SUCCESS) {
+        let fecha = new Date();
+        console.log(COLOR.GREEN, '[SUCCESS][' + fecha.toLocaleString() + '] ' + content);
+    }
 }
 
 function debug(content){
-    if(process.env.LOG_DEBUG) console.log('[DEBUG]' + content);
+    if(process.env.LOG_DEBUG) {
+        let fecha = new Date();
+        console.log('[DEBUG][' + fecha.toLocaleString() + '] ' + content);
+    }
 }
 
 module.exports = {info, error, warn, correct, debug}
