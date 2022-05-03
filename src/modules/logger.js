272e07c84a313bb28d2a63e5684a7c380f2f1d09
OriolGarciaFores <ori_ugf@hotmail.com>
2022-05-03 16:41:31 +0200

    Actualización de nuevo fichero config en logs.

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -1,35 +1,36 @@
 const COLOR = require('../constants/consoleColors.js');
+const config = require('../../config');
 
 function info(content){
-    if(process.env.LOG_INFO) {
+    if(config.logs.infoEnable) {
         let fecha = new Date();
         console.log(COLOR.BLUE, '[INFO][' + fecha.toLocaleString() + '] ' + content);
     }
 }
 
 function error(content){
-    if(process.env.LOG_ERROR) {
+    if(config.logs.errorEnable) {
         let fecha = new Date();
         console.log(COLOR.RED, '[ERROR][' + fecha.toLocaleString() + '] ' + content);
     }
 }
 
 function warn(content){
-    if(process.env.LOG_WARN) {
+    if(config.logs.warnEnable) {
         let fecha = new Date();
         console.log(COLOR.YELLOW, '[WARN][' + fecha.toLocaleString() + '] ' + content);
     }
 }
 
 function correct(content){
-    if(process.env.LOG_SUCCESS) {
+    if(config.logs.sucessEnable) {
         let fecha = new Date();
         console.log(COLOR.GREEN, '[SUCCESS][' + fecha.toLocaleString() + '] ' + content);
     }
 }
 
 function debug(content){
-    if(process.env.LOG_DEBUG) {
+    if(config.logs.debugEnable) {
         let fecha = new Date();
         console.log('[DEBUG][' + fecha.toLocaleString() + '] ' + content);
     }
