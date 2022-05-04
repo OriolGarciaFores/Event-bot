c71dc51b14839a986031ac370e05dfb4e8d79020
OriolGarciaFores <ori_ugf@hotmail.com>
2022-05-04 16:53:59 +0200

    v1.5.1: Añadido nuevo campo en /notice para añadir menciones.

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ module.exports = {
         sucessEnable: process.env.LOG_SUCCESS
     },
     status: {
-        version: '1.5.0',
+        version: '1.5.1',
         description: '/help | Version: ',
         type: 'PLAYING'
     }
--- src/slashCommands/notice.js
+++ src/slashCommands/notice.js
@@ -1,6 +1,7 @@
 const color = require('../constants/colors.js');
 const constant = require('../constants/constants.js');
 const utils = require('../modules/Utils.js');
+const log = require('../modules/logger');
 
 module.exports = {
 	slash : {
@@ -17,6 +18,12 @@ module.exports = {
             {
                 name : "description",
                 description : "Descripción del mensaje a enviar.",
+                type : constant.SLASH_OPTION_TYPE_STRING,
+                required : false
+            },
+            {
+                name : "menciones",
+                description : "Para añadir lista de menciones que notificar (@everyone, @rol, @usuario, etc...).",
                 type : constant.SLASH_OPTION_TYPE_STRING,
                 required : false
             },
@@ -41,6 +48,8 @@ module.exports = {
         let descripcion = options.getString('description');
         let urlTitle = options.getString('url_title');
         let urlImage = options.getString('url_img');
+        let menciones = options.getString('menciones');
+        const mencionesList = menciones.split(/ +/);
 
         if(titulo == undefined && descripcion == undefined){
             await interaction.reply({content : 'Requiere informar minimo un título o descripción.', ephemeral: true});
@@ -53,10 +62,12 @@ module.exports = {
             embed.author.name = interaction.user.username;
             embed.author.icon_url = interaction.user.displayAvatarURL();
 
+            let mencionesValidas = await obtenerListaMenciones(client, mencionesList, interaction.guildId);
+
             if(utils.isImage(urlImage)) embed.image = { url: urlImage};
             if(utils.isUrl(urlTitle)) embed.url = urlTitle;
-    
-            await interaction.reply({embeds: [embed]});
+
+            await interaction.reply({embeds: [embed], content: mencionesValidas});
         }
 	},
 	async reactionAdd(reaction, user){
@@ -84,4 +95,33 @@ function initEmbed(){
         }
     }
     return embed;
+}
+
+async function obtenerListaMenciones(client, mencionesList, guildId){
+    let mencionesValidas = "";
+
+    for(let i = 0; i < mencionesList.length; i++){
+        if(mencionesList[i].includes('@')){
+            if(mencionesList[i] === '@everyone'){
+                mencionesValidas = mencionesValidas === "" ?  mencionesList[i] : mencionesValidas + " " + mencionesList[i];
+            }else{
+                let mencionId = mencionesList[i].replace(/[^0-9 ]/g, "").trim();
+
+                if(mencionId === "") continue;
+
+                let rol = await utils.findRol(client, guildId, mencionId);
+                let member = await client.users.fetch(mencionId).catch(err => log.debug('No se encuentra el usuario ' + mencionId));
+
+                if(rol){
+                    mencionesValidas = mencionesValidas === "" ?  mencionesList[i] : mencionesValidas + " " + mencionesList[i];
+                }
+                
+                if(member){
+                    mencionesValidas = mencionesValidas === "" ?  mencionesList[i] : mencionesValidas + " " + mencionesList[i];
+                }
+            }
+        }
+    }
+
+    return mencionesValidas;
 }
