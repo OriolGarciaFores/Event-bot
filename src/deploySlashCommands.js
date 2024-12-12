6ee32d96ef107d2b6a1092bd7080a9487e6ccc88
Tebrase <ori_ugf@hotmail.com>
2024-12-12 20:50:26 +0100

    Refactorización deploySlashCommands

--- src/deploySlashCommands.js
+++ src/deploySlashCommands.js
@@ -1,4 +1,3 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
@@ -7,19 +6,7 @@ const log = require('./modules/logger');
 
 require("dotenv").config();
 
-
-/*
-
-const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
-*/
-
 const commands = []
-
 const commandFiles = fs.readdirSync('./src/slashCommands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
@@ -29,8 +16,15 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
-//applicationCommands -> GLOBAL Tarda 1 hora al añadirse a los servers.
+
 //applicationGuildCommands -> Solo para un server especifico, se añade inmediato
+/*
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID_DEV), { body: commands })
+	.then(() => log.correct('Successfully registered application commands.'))
+	.catch(console.error);
+*/
+
+//applicationCommands -> GLOBAL Tarda 1 hora al añadirse a los servers.
 rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
 	.then(() => log.correct('Successfully registered application commands.'))
 	.catch(console.error);
