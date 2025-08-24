56a25b5e45cf703d984440ca587e60afab07e34b
OriolGarciaFores <ori_ugf@hotmail.com>
2025-08-24 16:54:35 +0200

    Corrección local deploy

--- src/deploySlashCommands.js
+++ src/deploySlashCommands.js
@@ -27,11 +27,11 @@ const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
 
 //applicationGuildCommands -> Solo para un server especifico, se añade inmediato
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID_DEV), { body: commands })
-	.then(() => log.correct('Successfully registered application commands.'))
-	.catch(console.error);
-
-//applicationCommands -> GLOBAL Tarda 1 hora al añadirse a los servers.
-/*rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
+/*rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID_DEV), { body: commands })
 	.then(() => log.correct('Successfully registered application commands.'))
 	.catch(console.error);*/
+
+//applicationCommands -> GLOBAL Tarda 1 hora al añadirse a los servers.
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
+	.then(() => log.correct('Successfully registered application commands.'))
+	.catch(console.error);
